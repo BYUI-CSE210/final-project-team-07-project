b88d209983b5f83a9ce40aa8786c1c5ad69c0465
KevinPena2022 <pen21006@byui.edu>
2022-07-06 22:26:24 -0700

    First Edits Kevin Pena

---
+++ flyingObject.py
@@ -0,0 +1,9 @@
+class Point:
+  def __init__(self):
+    self.x = 0.0
+    self.y = 0.0
+
+class Velocity:
+  def __init__(self):
+    self.dx = 0.0
+    self.dy = 0.0
---
+++ main.py
@@ -0,0 +1,11 @@
+from player import Player
+
+
+def main():
+    p1 = Player()
+    p1.drawing_player()
+
+
+
+if __name__ == "__main__":
+    main()
